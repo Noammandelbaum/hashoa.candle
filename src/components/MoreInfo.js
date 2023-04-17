1efc123714f08630ae9ced62288b4d45d40446dd
Noam <noam.mandelbaum@gmail.com>
2023-04-17 11:19:41 +0300

    Copyright

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -1,90 +1,98 @@
-import { Box, Link, Typography } from '@mui/material';
+import { Box, Divider, Link, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 const margin = 3;
 
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props} >
+      {'פיתוח האתר - '}
+      <Link color="inherit" onClick={() => window.location = 'mailto:noam.mandelbaum@gmail.com'}>
+        {'נעם מנדלבאום'}
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
 export default function MoreInfo() {
   return (
-    <Container
-      sx={{
-        pr: 7, pl: 0,
-        '@media (min-width: 600px)': {
-          pr: 7, pl: 1,
-        }
-      }}>
-      <Box
+    <>
+      <Container
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          bgcolor: '#005C99',
-          mt: margin,
-        }}
-      >
+          pr: 7, pl: 0,
+          '@media (min-width: 600px)': {
+            pr: 7, pl: 1,
+          }
+        }}>
         <Box
-          display="flex"
-          flexDirection="column-reverse"
-          justifyContent="flex-end"
-          sx={{ mr: 3 }}
-        />
-        <Box       >
-          <Typography
-            variant="h8"
-            component="div"
-            style={{
-              color: '#ffffff',
-            }}
-          >
-            {"מצטרפים ומדליקים נר:"}
-          </Typography>
-          <Typography
-            variant="h8"
-            component="div"
-            style={{
-              color: '#ffffff',
-            }}
-          >
-            {"• כדי לזכור את אלה שאינם"}
-          </Typography>
-          <Typography
-            variant="h8"
-            component="div"
-            style={{
-              color: '#ffffff',
-            }}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            bgcolor: '#005C99',
+            mt: margin,
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="column-reverse"
+            justifyContent="flex-end"
+            sx={{ mr: 3 }}
+          />
+          <Box       >
+            <Typography
+              variant="h8"
+              component="div"
+              style={{
+                color: '#ffffff',
+              }}
+            >
+              {"מצטרפים ומדליקים נר:"}
+            </Typography>
+            <Typography
+              variant="h8"
+              component="div"
+              style={{
+                color: '#ffffff',
+              }}
+            >
+              {"• כדי לזכור את אלה שאינם"}
+            </Typography>
+            <Typography
+              variant="h8"
+              component="div"
+              style={{
+                color: '#ffffff',
+              }}
 
-          >
-            {"• ממשיכים לפעול להנצחת זכרון השואה"}
-          </Typography>
-          <Typography
-            variant="h8"
-            component="div"
-            style={{
-              color: '#ffffff',
-            }}
-          >
-            {`• נר ועוד נר לחיבור גדול`}
-          </Typography>
-          <Typography
-            variant="h8"
-            component="div"
-            sx={{ mb: margin }}
-            style={{
-              color: '#ffffff',
-            }}
-          >
-            {`• להוספת שם לחצו על סימן העיפרון מימין`}
-          </Typography>
+            >
+              {"• ממשיכים לפעול להנצחת זכרון השואה"}
+            </Typography>
+            <Typography
+              variant="h8"
+              component="div"
+              style={{
+                color: '#ffffff',
+              }}
+            >
+              {`• נר ועוד נר לחיבור גדול`}
+            </Typography>
+            <Typography
+              variant="h8"
+              component="div"
+              sx={{ mb: margin }}
+              style={{
+                color: '#ffffff',
+              }}
+            >
+              {`• להוספת שם לחצו על סימן העיפרון מימין`}
+            </Typography>
+          </Box>
+
         </Box>
-        {/* <Link sx={{ mb: 1 }}
-          href="https://www.facebook.com/hashtag/%D7%9E%D7%93%D7%9C%D7%99%D7%A7%D7%99%D7%9D_%D7%A0%D7%A8?paipv=0&eav=Afa6id6Sn5JhavJmfc2YAwBIMq5g9maCMQEIygRgRFT6A69hqoi0V7hhrTFuQ2-M_pE"
-        >
-          צפו בנרות שהודלקו:  #מדליקים_נר
-        </Link>
-        <Link
-          href="https://yakirli.org/awareness-week/"
-        >
-          למידע אודות שבוע המודעות לשכול האזרחי
-        </Link> */}
-      </Box>
-    </Container>
+      </Container>
+      <Divider flexItem sx={{ mr: -1, ml: -2 }} />
+      <Copyright sx={{ mt: 4, mb: 4 }} />
+    </>
   );
 }
